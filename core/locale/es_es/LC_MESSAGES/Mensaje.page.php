4f610f530976101545e3899c742fe3582a733870
Carlos <karrlyttos@hotmail.com>
2015-01-21 15:48:08 -0500

    Se agregan mejoras a la clase Mensaje y se agrega clase de persistencia

--- core/locale/es_es/LC_MESSAGES/Mensaje.page.php
+++ core/locale/es_es/LC_MESSAGES/Mensaje.page.php
@@ -9,4 +9,27 @@ $this->idioma ["noDefinido"] = "N/D";
 $this->idioma ["sesionNoExiste"] = "<h3>No se pudo rescatar una sesi√≥n!!!!</h3><p>Su sesi√≥n de trabajo ha expirado o no tiene permiso de acceder a este recurso.</p><p class='textoNegrita centrar'>En unos instantes ser√° redirigido a la p√°gina de inicio.</span></p>";
 $this->idioma ["errorDatos"] = "<h3>Datos no v√°lidos.</h3><p>Por favor revise los datos ingresados y vuelva a reintentar la operaci√≥n.</p>";
 
+
+//valores para la clase Persistencia.class.php
+
+$this->idioma["errorConexion"]="Error, FallÛ al conectar a la base de datos";
+$this->idioma["errorEliminar"]="FallÛ al eliminar registros";
+$this->idioma["errorConsulta"]="La tabla no tiene datos Û la consulta ha fallado";
+$this->idioma["errorElementosArray"] = "Error, el numero de elementos de los arrays deben ser iguales";
+$this->idioma["errorActualizar"] ="FallÛ al realizar la ActualizaciÛn";
+$this->idioma["errorInsertar"] ="FallÛ al realizar la inserciÛn";
+$this->idioma['errorProceso'] ="El usuario no poseÈ permisos sobre el proceso";
+$this->idioma["errorTablaNoExiste"] ="Error Tabla no Existe";
+$this->idioma["errorTablaHNoExiste"] ="Error Tabla Historico no Existe";
+$this->idioma["errorCampos"] ="Error Campos invalidos";
+$this->idioma["errorValores"] ="Error valores invalidos";
+$this->idioma["errorWhere"] ="Error sentencia where invalida";
+$this->idioma["errorColumnas"] ="Error no es posible consultar las columnas";
+$this->idioma["errorHistorico"] ="Error no es posible crear historico";
+$this->idioma["errorNoActualizar"] = "No existen elementos para actualizar";
+$this->idioma["errorPks"] = "Error no fue posible recuperar las llaves primarias";
+$this->idioma["errorConteo"] = "La tabla esta vacia Û no existe";
+
+//fin valores 
+
 ?>
