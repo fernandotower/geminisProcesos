3497d02db0ca8c44ceec304513686f0d6c4ac9f0
Carlos <karrlyttos@hotmail.com>
2015-02-02 17:25:15 -0500

    agrega base

---
+++ component/GestorProcesos/Modelo/Base.class.php
@@ -0,0 +1,71 @@
+<?php
+
+
+namespace component\GestorProcesos\Modelo;
+
+include_once ('core/connection/DAL.class.php');
+
+
+if (! isset ( $GLOBALS ["autorizado"] )) {
+	include ("../index.php");
+	exit ();
+}
+
+  
+
+class Base{
+	
+	
+
+	protected $dao =  NULL;
+
+	public $consultas =  array();
+	
+
+	public function __construct($conexion = ''){
+
+
+		if(!$this->setDataAccessObject($conexion)) return false;
+
+		 
+	}
+	
+	
+	
+	public function getConsultas(){
+		return $this->consultas;
+	}
+	
+	public function setDataAccessObject($conexion = ''){
+		
+		
+		if($conexion == '') return false;
+		$this->dao =  new \DAL();
+		$this->dao->setConexion($conexion);
+		
+		if(is_object($this->dao)) return true;
+		
+		return false;
+		
+	}
+	
+	public function unsetDao(){
+		unset($this->dao);
+	}
+	
+	public function __call($method_name, $arguments){
+		
+		if(is_null($this->dao)) return false;
+
+		if(method_exists ($this , $method_name )) 
+			return call_user_func_array(array($this , $method_name), $arguments);
+		
+		return  call_user_func_array(array($this->dao , $method_name), $arguments);
+		 
+		
+	}
+	
+	
+
+
+}
